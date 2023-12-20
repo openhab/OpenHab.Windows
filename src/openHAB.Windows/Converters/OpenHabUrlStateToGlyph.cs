645ac22789b496d48bab468aa74bfd69d5dd8f85
Christoph Hofmann <oss@hoffe.org>
2023-12-20 16:11:57 +0100

    Adopted Icon caching, notifications and UI for WinUI
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

--- src/openHAB.Windows/Converters/OpenHabUrlStateToGlyph.cs
+++ src/openHAB.Windows/Converters/OpenHabUrlStateToGlyph.cs
@@ -1,8 +1,8 @@
-using Microsoft.UI.Xaml.Data;
-using openHAB.Core.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.UI.Xaml.Data;
+using openHAB.Core.Model;
 
 namespace openHAB.Windows.Converters
 {
